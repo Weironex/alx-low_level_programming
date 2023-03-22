b3e49e8b4e58352f5279a174039af2609ea1f081
Adenugba Mary <adenugbamary6@gmail.com>
2023-03-21 19:38:44 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @i: The first parameter to be added.
+ * @j: The second parameter to be added.
+ *
+ * Return: The result of the addition.
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
