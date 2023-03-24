f658c29150924bd3fcad4cf3c24cf759dc28fccc
Adenugba Mary <adenugbamary6@gmail.com>
2023-03-24 16:18:22 -0700

    Numbers constitute the only universal language mandatory iii

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints 10 times the numbers, from 0 to 14,
+ *			followed by a new line.
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int n, c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+				_putchar('0' + n / 10);
+			_putchar('0' + n % 10);
+		}
+		_putchar('\n');
+	}
+}
