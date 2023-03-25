6f8356b996b674329c1f6ad20cb7616b9b08d349
Weironex <126628203+Weironex@users.noreply.github.com>
2023-03-25 02:04:12 +0100

    Prime factors

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Finds and prints the largest prime factor of the number 612852475143, 
+ * main - Finds and prints the largest prime factor of the number 612852475143,
  *        followed by a new line.
  *
  * Return: Always 0
