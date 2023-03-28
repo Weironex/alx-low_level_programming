6da2e2395249eca7270a4cc37b4dc37eb8a046f4
Adenugba Mary <adenugbamary6@gmail.com>
2023-03-28 14:17:37 -0700

    Half the lies they tell about me are not true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,18 @@
 
 void puts2(char *str)
 {
-	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	int i = 0;
+
+	while (str[i] != '\0')
+
 	{
-		_putchar(str[i]);
+		if (i % 2 == 0)
+
+		{
+			_putchar(str[i]);
+		}
+
 		i++;
 	}
 
