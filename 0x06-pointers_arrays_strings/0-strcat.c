8a50afaaf020c30bf305aa1aefc6950e691b1dfe
Adenugba Mary <adenugbamary6@gmail.com>
2023-03-30 16:01:11 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,7 +5,7 @@
  * @dest: Destination string.
  * @src: The source string.
  *
- * Return: Destination string.
+ * Return: A pointer to the resulting string @dest
  */
 
 char *_strcat(char *dest, char *src)
