df0891397d4f2c758d500371fc00e5a02707914e
Adenugba Mary <adenugbamary6@gmail.com>
2023-03-30 16:05:29 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,15 +10,14 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int i = 0;
 	int destlen = 0;
-	int srclen  = 0;
-	int i;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	while (dest[i++])
 		destlen++;
-	for (i = 0; srclen[i] != '\0'; i++)
-		srclen++;
-	for (i = 0; i <= srclen; i++)
-		dest[destlen + i] = src[i];
+
+	for (i = 0; src[i]; i++)
+		dest[destlen++] = src[i];
+
 	return (dest);
 }
