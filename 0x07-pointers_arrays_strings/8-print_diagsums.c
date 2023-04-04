88510705c5929c53d4d7a7f92b8d791fa96b2f2b
Weironex <126628203+Weironex@users.noreply.github.com>
2023-04-04 21:37:51 +0100

    The line of life is a ragged diagonal between duty and desire

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,11 +12,10 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i;
 	int sum1 = 0;
 	int sum2 = 0;
 
-	for (i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
 		sum1 += *(a + i * size + i);
 		sum2 += *(a + i * size + (size - i - 1));
