d5c6c19192e05d65794f9eedf1fbeb8ba21a8efc
Adenugba Mary <adenugbamary6@gmail.com>
2023-04-06 22:16:30 -0700

    Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,8 +2,7 @@
 int num_prime(int n, int i);
 
 /**
- * is_prime_number - Returns 1 if the input integer is a prime number,
- *			otherwise return 0.
+ * num_prime - number is  prime?
  * @n: Parameter function
  * @i: Parameter function
  *
@@ -28,10 +27,11 @@ int num_prime(int n, int i)
 }
 
 /**
- * is_prime_number - number is prime?
+ * is_prime_number - Returns 1 if the input integer is a prime number,
+ *			otherwise return 0.
  * @n: Parameter funtion
  *
- * Return: recursion
+ * Return: 1 if the input integer is a prime number, otherwise return 0.
  */
 
 int is_prime_number(int n)
