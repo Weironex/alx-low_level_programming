4085e9c77a50a0d1c32cc8fa2d0e31d6ab031e94
Weironex <126628203+Weironex@users.noreply.github.com>
2023-04-07 07:11:46 +0100

    Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * wildcmp - Compares two strings and returns 1 if the strings can be 
+ * wildcmp - Compares two strings and returns 1 if the strings can be
  *		considered identical, otherwise return 0.
  * @s1: Parameter function
  * @s2: Parameter function
