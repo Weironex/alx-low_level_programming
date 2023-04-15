fde27e61c23e61009f99986fff9ccbb574c7e41e
Adenugba Mary <adenugbamary6@gmail.com>
2023-04-14 22:30:23 -0700

    Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,9 +7,12 @@
  *
  * Return: A pointer to the allocated memory.
  */
+
 void *malloc_checked(unsigned int b)
 {
-	void *p = malloc(b);
+	void *p;
+	
+	p = malloc(b);
 
 	if (p == NULL)
 		exit(98);
