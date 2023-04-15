a652e7f7c25ae19a6635c1f7c34062c5c1206dba
Adenugba Mary <adenugbamary6@gmail.com>
2023-04-14 22:08:54 -0700

    Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,20 +3,16 @@
 
 /**
  * malloc_checked - Allocates memory using malloc.
- * @b: The number of bytes.
+ * @b: The number of bytes to be allocated.
  *
- * Return: A  pointer to the allocated memory
+ * Return: A pointer to the allocated memory.
  */
-
 void *malloc_checked(unsigned int b)
 {
 	void *p = malloc(b);
 
 	if (p == NULL)
-	{
 		exit(98);
-	}
 
 	return (p);
-
 }
