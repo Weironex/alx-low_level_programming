50bc3bfe32d6a04c756fec2629eb5c74f7b02ed9
Weironex <126628203+Weironex@users.noreply.github.com>
2023-04-18 22:02:52 +0100

    Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,7 +12,7 @@
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  */
- 
+
 struct dog
 {
 	char *name;
@@ -23,7 +23,7 @@ struct dog
 /**
  * dog_t - Typedef for struct dog
  */
- 
+
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
