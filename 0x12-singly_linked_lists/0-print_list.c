ad4f9dfbac2f8be147b2423faa4027b42c3a894c
Weironex <126628203+Weironex@users.noreply.github.com>
2023-04-28 21:40:09 +0100

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,27 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_list - Prints all elements of a list_t list.
- * @h: Pointer to the beginning of the list.
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
  *
- * Return: The number of nodes in the list.
+ * Return: The number of nodes in h.
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
-		count++;
+		nodes++;
 		h = h->next;
 	}
 
-	return (count);
+	return (nodes);
 }
