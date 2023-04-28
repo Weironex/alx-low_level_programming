5eea0cc6d6580cb6c4ec797085f38d28bec701f1
Adenugba Mary <adenugbamary6@gmail.com>
2023-04-28 13:32:35 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,21 +7,21 @@
  *
  * Return: The number of nodes in the list.
  */
- 
+
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            _printf("[0] (nil)\n");
-        else
-            _printf("[%d] %s\n", h->len, h->str);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			_printf("[0] (nil)\n");
+		else
+			_printf("[%d] %s\n", h->len, h->str);
 
-        count++;
-        h = h->next;
-    }
+		count++;
+		h = h->next;
+	}
 
-    return (count);
+	return (count);
 }
