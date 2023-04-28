7bc626889e0aafce5d29c0307121073877ca6e9f
Weironex <126628203+Weironex@users.noreply.github.com>
2023-04-28 21:45:10 +0100

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,24 +4,22 @@
 /**
  * print_list - Prints all the elements of a list_t list.
  * @h: The list_t list.
- *
- * Return: The number of nodes in h.
+ * Return: The number of nodes.
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t count = 0;
 
 	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
+			printf("[%u] %s\n",  h->len, h->str);
+		count++;
 		h = h->next;
 	}
 
-	return (nodes);
+	return (count);
 }
