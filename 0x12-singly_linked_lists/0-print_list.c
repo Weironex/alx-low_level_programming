66d482d705bd68db7ce89f3b9f9b012e56154230
Weironex <126628203+Weironex@users.noreply.github.com>
2023-04-28 21:37:06 +0100

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,9 +15,9 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			_printf("[0] (nil)\n");
+			printf("[0] (nil)\n");
 		else
-			_printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 
 		count++;
 		h = h->next;
