6f2139f18bb74a3eae3ee869ecd70560128f5b09
Weironex <126628203+Weironex@users.noreply.github.com>
2023-05-03 03:03:46 +0100

    Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,54 +1,90 @@
 #include "lists.h"
 #include <stdio.h>
-#include <stdlib.h>
+
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
 
 /**
- * print_listint_safe - Prints a listint_t linked list.
- * @head: Pointer to the head node of the list.
+ * looped_listint_len - Counts the number of unique nodes
+ *                      in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ *
+ * Return: If the list is not looped - 0.
+ *         Otherwise - the number of unique nodes in the list.
+ */
+
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *tortoise, *hare;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+
+			return (nodes);
+		}
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+
+	return (0);
+}
+
+/**
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
  *
  * Return: The number of nodes in the list.
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *tortoise, *hare;
-	size_t count = 0;
+	size_t nodes, index = 0;
 
-	if (head == NULL)
-		exit(98);
+	nodes = looped_listint_len(head);
 
-	tortoise = head;
-	hare = head;
-
-	while (tortoise != NULL && hare != NULL && hare->next != NULL)
+	if (nodes == 0)
 	{
-		tortoise = tortoise->next;
-		hare = hare->next->next;
-
-
-		if (tortoise == hare)
+		for (; head != NULL; nodes++)
 		{
-			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
-			exit(98);
-		}
-
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-		count++;
-	}
-
-	if (tortoise != NULL)
-	{
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-		count++;
-		tortoise = tortoise->next;
-
-		while (tortoise != NULL)
-		{
-			printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-			count++;
-			tortoise = tortoise->next;
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
 	}
 
-	return (count);
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+
+	return (nodes);
 }
